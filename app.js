4005aa2efed85977536a036ebd930987b2e61597
nyankson-benjamin <117347759+nyankson-benjamin@users.noreply.github.com>
2023-09-15 15:05:44 +0000

    create e-commerce endpoints

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const usersRoute = require("./routes/allUsersRoute");
+const productRoute = require("./routes/productsRoute");
+const authRoute = require("./routes/authRoute");
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+app.use(usersRoute);
+app.use(productRoute);
+app.use(authRoute);
+module.exports = app;
